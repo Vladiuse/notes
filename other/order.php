a463a67a05da5282fb41b37b6759329c62366023
vladiuse <vladiuse@gmail.com>
2021-12-14 12:43:52 +0300

    keitaro script

--- other/order.php
+++ other/order.php
@@ -2,4 +2,5 @@
 // ЗАПИСЬ В LOG.TXT
 $today = date("m.d.y H:i:s");
 $message = $_POST['name'] . ";" . $_POST['phone'] . ";" . $today . ";" . $response['status'] . "\n";
+file_put_contents('log.txt', $message, FILE_APPEND | LOCK_EX);
 ?>
