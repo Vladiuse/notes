47c027e20a374ca01b4bcdc127a8f06ba3081726
vladiuse <vladiuse@gmail.com>
2022-05-04 14:02:24 +0300

    changes

--- buttons_app/js/main.js
+++ buttons_app/js/main.js
@@ -1,7 +1,12 @@
 let toogleSpeed = 150;
+let SITE = 'http://127.0.0.1:8000/'
+let mainActive = null;
+let subSpiner = $('#spin')
+let subBlock = $('#sub-buttons')
+
 
 // добавление данных
-$('.add').on('click', function(){
+$('.plus').on('click', function(){
     let area = $(this).attr('area')
     openForm(area)
     if ($(this).hasClass('active') == true){
@@ -35,18 +40,21 @@ function openForm(name){
 
 // load main-buttons
 function loadMainButton(){
-    URL = 'https://handy-fun.com/copy_paste';
+    
+    URL = SITE + 'copy_paste';
     $.ajax({
         url: URL,
         method: 'GET'
     }).done(function(data){
         let butons = data['buttons']
+        $('#main-buttons .spinner-border').remove()
         for (buttonID in butons){
             buttonData = butons[buttonID]
             let button = $('<button class="btn btn-primary main-bnt" id=""></button>')
             button.attr('id', buttonData['id'])
             button.text(buttonData['name'])
             $('#main-buttons').append(button)
+            
         }
         
     })
@@ -56,6 +64,15 @@ loadMainButton()
 
 // при нажатие на кнопку закрузить под-кнопки
 $('#main-buttons').on('click', 'button', function(){
+    if ($(this).hasClass('btn-primary')){
+        $(this).removeClass('btn-primary')
+        $(this).addClass('btn-outline-primary')
+    }
+    else{
+        $(this).addClass('btn-primary')
+        $(this).removeClass('btn-outline-primary')
+    }
+    mainActive = $(this).attr('id');
     let groupId = $(this).attr('id');
     loadSubButton(groupId)
     $('#text-data textarea').text('')
@@ -64,7 +81,10 @@ $('#main-buttons').on('click', 'button', function(){
 // load sub-buttons
 function loadSubButton(groupId){
     $('#sub-buttons').empty()
-    URL  = 'https://handy-fun.com/copy_paste/code_examples_data?group_id='
+    let spin = subSpiner.clone()
+    subBlock.append(spin)
+    spin.show();
+    URL  = SITE + 'copy_paste/code_examples_data?group_id='
     $.ajax({
         url: URL + groupId,
         method: 'GET'
@@ -72,6 +92,7 @@ function loadSubButton(groupId){
         subButtonsData = data['buttons']
         // console.log(subButtonsData, 'суб-кнопки')
         addSubs(subButtonsData)
+        spin.remove();
     })
 }
 
@@ -92,4 +113,55 @@ function addSubs(data){
 $('#sub-buttons').on('click', 'button', function(){
     let subButtonText = $(this).attr('value');
     $('#text-data textarea').text(subButtonText)
+})
+
+$('.add-main .add').click(function(){
+    createButton();
+})
+// Создать новую кнопку
+function createButton(name){
+    URL = SITE + 'copy_paste/add_group';
+    let newBtnName = $('#main input').val()
+    console.log(newBtnName)
+    data = {'group_name': newBtnName}
+    let res = $.post(URL, data, function(data){
+        console.log(data)
+    })
+    // $.ajax({
+    //     url: URL,
+    //     type: 'post',
+    //     data: {1: 1, 2: 2}
+    // }).done(function(response){
+    //     console.log(response)
+    // })
+}
+
+
+$('.add-sub .add').click(function(){
+    createSubButton();
+})
+
+// создать субКнопку
+function createSubButton(){
+    URL = SITE + 'copy_paste/add_sub_button';
+    console.log(mainActive);
+    let groupID = mainActive;
+    let subBtnName = $('.add-sub .sub-name').val();
+    let value = $('.add-sub .sub-value').val();
+    data = {
+        'group_id': groupID,
+        'name': subBtnName,
+        'data': value,
+    }
+    let res = $.post(URL, data, function(data){
+        console.log(data)
+    })
+}
+
+
+
+$('#test').click(function(){
+    console.log(123)
+    $('#text').css('background-color', 'green')
+    $('#text').css('align-items', 'flex-end')
 })
