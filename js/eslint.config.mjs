5dfabff495ba94cc63d9c56e246d269f2045d06b
vladiuse <vladiuse@gmail.com>
2025-03-12 14:11:08 +0300

    add fix

--- js/eslint.config.mjs
+++ js/eslint.config.mjs
@@ -5,11 +5,29 @@ import html from "eslint-plugin-html"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "script",
+      globals: globals.browser
+    },
+  },
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   {
     files: ["**/*.html"],
     plugins: { html },
+    rules: {
+      "no-unused-vars": "warn"
+    }
   },
+  {
+    files: ["**/*.js"],
+    // Уберите использование pluginJs.configs.recommended, чтобы не было конфликта
+    // или добавьте сюда нужные правила, если необходимо
+    rules: {
+      "no-unused-vars": "warn",  // Отключаем для JS файлов
+    }
+  }
+
 ];
