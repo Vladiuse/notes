7252a85d38da2ff8ed0dd88fdfe99a1c744b45f1
vladiuse <vladiuse@gmail.com>
2025-12-06 16:21:08 +0300

    remove old file

--- other/order.php
+++
@@ -1,6 +0,0 @@
-<?php
-// ЗАПИСЬ В LOG.TXT
-$today = date("m.d.y H:i:s");
-$message = $_POST['name'] . ";" . $_POST['phone'] . ";" . $today . ";" . $response['status'] . "\n";
-file_put_contents('log.txt', $message, FILE_APPEND | LOCK_EX);
-?>
--- python/test.py
+++
@@ -1,6 +0,0 @@
-def transform(string):
-    li = string.split(',')
-    li = map(lambda iso: f'{iso}', li)
-    return list(li)
-
-print(transform(input()))
